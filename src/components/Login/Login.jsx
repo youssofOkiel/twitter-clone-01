687c48917b735b91a2e9bc35725aa8d63fa4e827
Your Name <abdelsatarahmed77777@gmail.com>
2022-07-24 16:33:48 +0200

    Add home Page (Feed and widgetFollow)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import firebase from 'firebase'
 import backdrop from '../../assets/backdrop.png'
 import './Login.css'
+import Spinner from '../elements/Spinner/Spinner'
 
 import validate from '../../helpers/inputValidator'
 import db from '../../firebase'
@@ -148,7 +149,7 @@ const Login = () => {
                         <h2>Log In</h2>
                         <input type="email"    name='email'    placeholder='Email'    value={loginState.email}    onChange={e=>setLoginState({...loginState, email:e.target.value})}/>
                         <input type="password" name='password' placeholder='Password' value={loginState.password} onChange={e=>setLoginState({...loginState, password:e.target.value})} required/>
-                        <button type='submit' className='button'>{loading? "<Spinner />" : 'Log in'}</button>
+                        <button type='submit' className='button'>{loading? <Spinner /> : 'Log in'}</button>
                         {error.length>0 && <div className='error'>{error}</div>}
                         <p className="signup">Don't have an account? <span onClick={toggleAuth}>Sign up</span></p>
                     </form>
@@ -164,7 +165,7 @@ const Login = () => {
                         <input type="password" name="password"        placeholder='Create Password' value={signupState.password}        onChange={(e)=>setSignupState({...signupState, password: e.target.value})}/>
                         <input type="password" name="confirmPassword" placeholder='Confirm Password'value={signupState.confirmPassword} onChange={(e)=>setSignupState({...signupState, confirmPassword: e.target.value})}/>
                         {error.length>0 && <div className='error'>{error}</div>}
-                        <button type='submit' className='button'>{loading? "<Spinner />" : 'Sign up'}</button>
+                        <button type='submit' className='button'>{loading? <Spinner /> : 'Sign up'}</button>
                         <p className="signup">have an account? <span onClick={toggleAuth}>Log in</span></p>
                     </form>
                 </div>
