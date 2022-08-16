d5a0030ac44f6cf604e12802c3270bd30fd5b175
youssofOkiel <y.okiel@bimsaudi.com>
2022-08-16 14:13:48 +0200

    added some changes

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -3,30 +3,32 @@ import React, { useState, useEffect } from "react";
 import "./search.css";
 import db from "../../firebase";
 import UserItemFollow from "../UserItem/UserItemFollow";
+import { useStateValue } from "../../contexts/StateContextProvider";
 
 const Search = () => {
   const [searchName, setSearchName] = useState("");
   const [users, setUsers] = useState([]);
+  const profile = localStorage.getItem("twitter_user");
 
   const getname = (e) => {
     setSearchName(e.target.value);
-    console.log(searchName);
   };
 
   useEffect(() => {
-    db.collection("users")
-      .where("displayName", "==", searchName)
-      .onSnapshot((snapshot) => {
-        setUsers(
-          snapshot.docs.map((user) => ({
-            id: user.id,
-            ...user.data(),
-          }))
-        );
-      });
+    if (searchName != "") {
+      db.collection("users")
+        .where("displayName", ">", searchName)
+        .limit(3)
+        .onSnapshot((snapshot) => {
+          setUsers(
+            snapshot.docs.map((user) => ({
+              id: user.id,
+              ...user.data(),
+            }))
+          );
+        });
+    }
   }, [searchName]);
-
-  
 
   return (
     <>
@@ -35,10 +37,13 @@ const Search = () => {
           InputLabelProps={{ style: { fontSize: 13 } }}
           placeholder="search"
           size="small"
-          InputProps={{ style: { fontSize: 13 , borderRadius:'2rem' } }}
+          InputProps={{ style: { fontSize: 13, borderRadius: "2rem" } }}
           fullWidth
           value={searchName}
           onChange={(e) => getname(e)}
+          onEmptied={() => {
+            setSearchName("");
+          }}
           variant="outlined"
           id="fullWidth"
         />
@@ -46,7 +51,11 @@ const Search = () => {
       <div className="users_container">
         {users.length > 0 &&
           users.map((user) => {
-            return <UserItemFollow display={user} />;
+            if (searchName != "") {
+              if (user.displayName != JSON.parse(profile).displayName) {
+                return <UserItemFollow display={user} />;
+              }
+            }
           })}
       </div>
     </>
